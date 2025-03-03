d7aa160276bb4aa15ce7721e998085d0e60f7bbb
syc <1101175200@qq.com>
2025-03-03 23:30:13 +0800

    update: eslintConfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-children-prop": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@next/next/no-img-element": "off",
+      "jsx-a11y/alt-text": "off",
+      "react/display-name": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-unused-expressions": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
